4f3babed0534a352ccd08d3b2ba797c65a24e53b
DejanAcimovic <dacimovic1@etf.unsa.ba>
2019-03-23 12:07:25 +0100

    Intial project setup

---
+++ PictureModule/src/main/java/com/socialnetwork/pictureModule/PictureModule/PictureModuleApplication.java
@@ -0,0 +1,13 @@
+package com.socialnetwork.pictureModule.PictureModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureModuleApplication.class, args);
+	}
+
+}
---
+++ PictureModule/src/test/java/com/socialnetwork/pictureModule/PictureModule/PictureModuleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.socialnetwork.pictureModule.PictureModule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PictureModuleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
