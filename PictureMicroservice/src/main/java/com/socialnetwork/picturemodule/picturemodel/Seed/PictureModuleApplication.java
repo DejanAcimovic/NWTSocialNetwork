0510edb6ce69bf4186359ca874a7fbe6e346fd3f
nadzzzo <nzilic1@etf.unsa.ba>
2019-03-27 16:53:40 +0100

    Added PictureMicroservice

---
+++ PictureMicroservice/src/main/java/com/socialnetwork/picturemodule/picturemodel/Seed/PictureModuleApplication.java
@@ -0,0 +1,13 @@
+package com.socialnetwork.picturemodule.picturemodel.Seed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureModuleApplication.class, args);
+    }
+
+}
