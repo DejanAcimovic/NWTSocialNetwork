836751c52b5eccaab1dcda494dc867100d2aa6cb
DejanAcimovic <dacimovic1@etf.unsa.ba>
2019-03-27 18:40:34 +0100

    first endpoint

--- PostModule/src/main/java/com/socialnetwork/postModule/PostModule/Controllers/PostController.java
+++ PostModule/src/main/java/com/socialnetwork/postModule/PostModule/Controllers/PostController.java
@@ -2,18 +2,27 @@ package com.socialnetwork.postModule.PostModule.Controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.socialnetwork.postModule.PostModule.Entities.Post;
+import java.util.List;
 
+import com.socialnetwork.postModule.PostModule.Entities.Post;
+import com.socialnetwork.postModule.PostModule.Services.PostService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RestController
 public class PostController {
 
-    @RequestMapping(value="/posts", method=RequestMethod.GET)
-    public Post getPost() {
-        return new Post(1, "laksjdflkajfsd");
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value="/posts/{userId}", method=RequestMethod.GET)
+    public @ResponseBody List<Post> getPostByUserId(@PathVariable("userId") Integer userId) {
+        return service.getPostByUserId(userId);
     }
     
 }
---
+++ PostModule/src/main/java/com/socialnetwork/postModule/PostModule/Services/PostService.java
@@ -0,0 +1,26 @@
+package com.socialnetwork.postModule.PostModule.Services;
+
+import java.util.List;
+
+import com.socialnetwork.postModule.PostModule.Entities.Post;
+import com.socialnetwork.postModule.PostModule.Repositories.PostRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PostService {
+
+    private PostRepository repository;
+
+    @Autowired
+    public PostService(PostRepository _repository){
+        this.repository = _repository;
+    }
+
+    public List<Post> getPostByUserId(Integer userId){
+        return repository.findByUserId(userId);
+    }
+    
+}
