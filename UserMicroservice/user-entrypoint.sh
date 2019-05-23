af30d23f4e14578eb2d6227e3f572a11820319ef
DejanAcimovic <dacimovic1@etf.unsa.ba>
2019-05-23 16:46:22 +0200

    docker compose bez komunikacije medju servisima

--- UserMicroservice/user-entrypoint.sh
+++ UserMicroservice/user-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-java -jar /opt/lib/usermodule-0.0.1-SNAPSHOT.jar
+while ! nc -z eureka-server 8761 ; do
+    echo "Waiting for the Eureka Server"
+    sleep 3
+done
+java -jar -Xmx128m /opt/lib/usermodule-0.0.1-SNAPSHOT.jar
